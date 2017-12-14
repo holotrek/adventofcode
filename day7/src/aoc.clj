eab2eeb1368f4afb51859abe46a1b123e86b3695
Evan Kaiser <evshell18@gmail.com>
2017-12-14 07:23:57 -0500

    Started day7. Got basic Clojure namespacing working.

---
+++ day7/src/aoc.clj
@@ -0,0 +1,5 @@
+(ns aoc
+    (:require [aoc.readfile :as rf] ))
+    
+(defn -main [inputFile]
+    (print (rf/read-data inputFile)))
---
+++ day7/src/aoc/readfile.clj
@@ -0,0 +1,6 @@
+(ns aoc.readfile
+    (:require [clojure.java.io :as io] ))
+
+(defn read-data [inputFile]
+    (def data-file (io/reader inputFile))
+    (slurp data-file))
