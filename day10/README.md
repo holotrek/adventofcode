51650b61cfefa00787cc9b0cdd2cdd8a9496eb38
evshell18 <evshell18@gmail.com>
2017-12-17 14:12:53 -0500

    Finished Day 10

---
+++ day10/README.md
@@ -0,0 +1,26 @@
+# adventofcode
+Solutions for Advent of Code
+
+## Prerequisites
+
+NodeJS >= 6.11.0
+
+## Install
+
+```
+npm i
+```
+
+## Day4
+
+Usage:
+```
+npm start -- -p 2 -f puzzleInput.txt
+```
+
+Arguments:
+* -p: The part of the day to execute (1 or 2)
+* -l: The length of the hash in bytes (defaults to 256)
+* -f: The file containing the key to input
+* -k: The key if directly entered instead of using a file
+
---
+++ day10/index.ts
@@ -0,0 +1,60 @@
+import { Command } from 'commander';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { KnotHash } from './knot-hash';
+
+const program = new Command();
+program
+    .version('0.0.1')
+    .option('-l, --length <length>', 'Indicates the [length] of the hash in bytes. Default: 256')
+    .option('-p, --part <part>', 'Identifies which [part] of the puzzle to execute. Values: 1 or 2. Default: 1')
+    .option('-f, --file <file>', 'The [file] to use for puzzle input (key). Overrides use of the -k/--key flag.')
+    .option('-k, --key <key>', 'The puzzle input [key] if not using a file. Defaults to empty.')
+    .parse(process.argv);
+
+const part1 = (data: string, length: number) => {
+    const input = data.split(',');
+    const hash = new KnotHash(length);
+    for (const len of input) {
+        hash.twist(+len);
+    }
+    console.log(hash.values[0] * hash.values[1]);
+};
+
+const part2 = (data: string, length: number) => {
+    const salt = [17, 31, 73, 47, 23];
+    const hash = new KnotHash(length);
+    console.log(hash.computeHash(data, salt));
+};
+
+const options = {
+    length: +program.length || 256,
+    part: +program.part || 1,
+    file: program.file,
+    key: program.key
+}
+
+if (options.file) {
+    fs.readFile(path.join(__dirname, '..', options.file), 'ascii', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            if (options.part === 1) {
+                part1(data.trim(), options.length);
+            }
+            else if (options.part === 2) {
+                part2(data.trim(), options.length);
+            }
+        }
+    });
+}
+else {
+    if (options.part === 1) {
+        part1(options.key || '', options.length);
+    }
+    else if (options.part === 2) {
+        part2(options.key || '', options.length);
+    }
+}
