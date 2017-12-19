1990713ae887bf2b772c12c28bbd00d2a43bf3fe
Evan Kaiser <evshell18@gmail.com>
2017-12-19 07:02:03 -0500

    Started writing part1 of day 15

---
+++ day15/README.md
@@ -0,0 +1,19 @@
+# adventofcode
+Solutions for Advent of Code
+
+## Prerequisites
+
+Python >= 2.7
+
+## Day 15
+
+To execute part1 of the day:
+
+```
+./part1.py <iterations> <generator1> <generator2>
+```
+
+To execute part2 of the day:
+
+```
+```
---
+++ day15/part1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) < 4:
+  print 'Invalid command: ' + ' '.join(sys.argv)
+  print '''
+  Usage:
+    ./part1.py <iterations> <generator1> <generator2>
+    iterations  - Number of generator iterations to compare.
+    generatorN	- The initial value of generator N.
+  '''
+
+iterations = long(sys.argv[1])
+gen1 = long(sys.argv[2])
+gen2 = long(sys.argv[3])
+
+gen1factor = 16807
+gen2factor = 48271
+div = 2147483647
+
+def generate(gen, factor, div):
+    return (gen*factor) % div
+
+def toBin(num):
+    return "{0:b}".format(num)
+
+matchCount = 0
+for i in range(iterations):
+    gen1 = generate(gen1, gen1factor, div)
+    gen2 = generate(gen2, gen2factor, div)
+
+    gen1Last16 = toBin(gen1)[-16:]
+    gen2Last16 = toBin(gen2)[-16:]
+
+    print gen1Last16
+    print gen2Last16
+    print ""
+    
+    if gen1Last16 == gen2Last16:
+        matchCount+=1
+
+print matchCount
