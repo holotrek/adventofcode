90ece9512b90372c7802a55218b9ea2f006b168f
Evan Kaiser <evshell18@gmail.com>
2017-12-24 10:23:38 -0500

    Finished day24 part1

--- day24/part1.py
+++ day24/part1.py
@@ -32,12 +32,37 @@ def makeLink(bridge, num, comp):
     bridge.append(comp)
     return other
 
-bridge = []
-lastNum = 0
-while True:
-    lnk = findLink(bridge, lastNum)
-    if lnk == None:
-        break
-    lastNum = makeLink(bridge, lastNum, lnk)
+def findBridge(bridges, search):
+    for b in bridges:
+        if set(b) == set(search):
+            return b
 
-print bridge
+def makeBridge(otherBridges):
+    bridge = []
+    lastNum = 0
+    while True:
+        lnk = findLink(bridge, lastNum)
+        if lnk == None:
+            break
+        lastNum = makeLink(bridge, lastNum, lnk)
+    if findBridge(otherBridges, bridge) == None:
+        return bridge
+    else:
+        return None
+
+bridges = []
+while True:
+    bridge = makeBridge(bridges)
+    if bridge == None:
+        break
+    bridges.append(bridge)
+
+max = 0
+for b in bridges:
+    parts = [p.split('/') for p in b]
+    nums = [int(item) for sublist in parts for item in sublist]
+    strength = sum(nums)
+    if strength > max:
+        max = strength
+
+print max
